30f42770657508989a69242f2d6e1e24c299fd3a
vladanrstc1se321 <59207837+vladanrstc1se321@users.noreply.github.com>
2019-12-25 17:38:48 +0100

    Dodate pages komponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './page-component/footer/footer.component';
+import { HeaderComponent } from './page-component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

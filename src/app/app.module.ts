3326d1e1ac00c3659e2d207f8e92d4a75d4314c0
vladanrstc1se321 <59207837+vladanrstc1se321@users.noreply.github.com>
2019-12-25 17:56:16 +0100

    Dodato rutiranje + html za stranice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './page-component/footer/footer.component';
 import { HeaderComponent } from './page-component/header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PostsContainerComponent } from './posts-container/posts-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    PostsContainerComponent
   ],
   imports: [
     BrowserModule,
